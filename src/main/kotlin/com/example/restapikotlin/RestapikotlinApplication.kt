48c545f15208d800c7171cc7b57c5050476e7bf5
fajar sidik priatna <jardik.oryza@gmail.com>
2023-09-11 11:13:51 +0700

    initial

---
+++ src/main/kotlin/com/example/restapikotlin/RestapikotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.restapikotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestapikotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestapikotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/restapikotlin/RestapikotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.restapikotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestapikotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
